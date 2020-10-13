1dc0c4660869214cbfdad4546d9e4e01e5068cd6
OLauterbach <olauterb@chicagobooth.edu>
2020-10-13 19:40:30 +0000

    RACV and Links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,23 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+  def rules
+  render ({ :template => "game_templates/rules.html.erb"})
+  end
+
+  def play_rock
+    render ({ :template => "game_templates/user_rock.html.erb"})
+    #render ({ :html => "<h1>Hello, World!</h1>".html_safe})
+    #redirect_to("https://www.wikipedia.com")
+  end
+
+  def play_paper
+    render ({ :template => "game_templates/user_paper.html.erb"})
+  end
+
+  def play_scissors
+    render ({ :template => "game_templates/user_scissors.html.erb"})
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
+  get("/", { :controller => "application", :action => "rules"})
+  get("/scissors", { :controller => "application", :action => "play_scissors"})
+  get("/paper", { :controller => "application", :action => "play_paper"})
+  get("/rock", { :controller => "application", :action => "play_rock"})
 
 end
